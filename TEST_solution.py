0c8f77b0ab64e0b909ce53be4860b9393dd15ead
syh0415 <129484708+syh0415@users.noreply.github.com>
2024-12-27 11:12:52 +0900

    Update TEST_solution.py

--- TEST_solution.py
+++ TEST_solution.py
@@ -1,3 +1,16 @@
+import sys
+from playwright.sync_api import Playwright, sync_playwright
+import telegram
+import asyncio
+import time
+
+sys.stdout.reconfigure(encoding='utf-8')
+
+# 봇 토큰과 채팅 ID 설정
+BOT_TOKEN = '7505977485:AAGv24lXugrL6gb5RlisRX9NvB3Zvc-QEAs'
+CHAT_ID = '7589059359'
+bot_message = ""
+
 N = int(input())
 A = list(map(int,input().split()))
 l,r = 0,N-1
@@ -16,3 +29,11 @@ while l<r and ans>0:
 print("good")
 print(p1,p2)
 
+async def send_bot_message_for_test():
+    message = "hello telegram, I am Yongho Shin"
+    print("bot msg - " + message)
+    bot = telegram.Bot(token=BOT_TOKEN)
+    await bot.send_message(CHAT_ID, message, "MARKDOWN")
+
+# asyncio.run(send_bot_message_with_numbers())  # 봇 실행하는 코드
+asyncio.run(send_bot_message_for_test())
